d438caef433eb8521fc36a35e86a36b7a0d21e80
YOUR NAME <joram.n.v@gmail.com>
2016-03-21 21:47:34 +0100

    first commit

---
+++ app/src/main/java/nl/visser/joram/mathapp/RandomAddition.java
@@ -0,0 +1,18 @@
+package nl.visser.joram.mathapp;
+
+/**
+ * Created by Joram on 21-3-2016.
+ */
+public class RandomAddition {
+    //TODO Make random addition calculations/sommetjes/opdrachten
+
+    public RandomAddition(int difficulty) {
+        if (difficulty == 1) {
+            //TODO set difficulties.
+        }
+    }
+
+    public String get(){
+        return "7 + 7 = ";
+    }
+}
